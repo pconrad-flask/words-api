a9a541a4c4bdbf1a81d6548444bfbaf997c45a20
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-08-24 06:31:26 -0700

    pc - update to use PORT env variable

--- main.py
+++ main.py
@@ -37,4 +37,5 @@ def renderPage3():
     return render_template('page3.html')
     
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0")
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True,host="0.0.0.0", port=port)
